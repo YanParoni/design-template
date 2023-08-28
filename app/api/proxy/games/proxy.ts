6fb939057578dc4c3183f5626c4bc6c78d917b28
YanParoni <paroniyan@gmail.com>
2023-08-28 02:24:56 -0300

    fix: fix more vercel not building errors

--- app/api/proxy/games/proxy.ts
+++ app/api/proxy/games/proxy.ts
@@ -2,7 +2,6 @@ import { inject, injectable } from 'inversify';
 import { IGamesGateway } from '@infra/gateways/contracts/games';
 import type { IHttpClient } from '@infra/http/contracts';
 
-import IQueryParams from '@infra/gateways/contracts/query';
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
