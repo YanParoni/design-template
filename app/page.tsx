1d2beb931762563e64dcaaf91a8aae443a63e256
YanParoni <paroniyan@gmail.com>
2023-09-11 01:20:32 -0300

    fix: fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,17 @@ import ThemeButton from "@ui/components/molecules/theme-button";
 import Pagination from "@ui/components/organisms/pagination";
 import CardLoadingSkeleton from "@ui/components/organisms/card/card-loading-skeleton";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined }
-}) {
-  const page = searchParams['page'] ?? '1'
-  const per_page = searchParams['per_page'] ?? '20'
-  const start = (Number(page) - 1) * Number(per_page)
-
+export default function Home() {
   return (
     <main className="bg-bkg">
-      <Suspense fallback={<div style={{color: 'white'}}>loading...</div>}>
-      <ThemeButton />
+      <Suspense fallback={<div style={{ color: 'white' }}>loading...</div>}>
+        <ThemeButton />
       </Suspense>
-      <Suspense fallback={<CardLoadingSkeleton/>}>
+      <Suspense fallback={<CardLoadingSkeleton />}>
         <List />
-        <Pagination   
-        hasNextPage={true}
-        hasPrevPage={start > 0}
+        <Pagination
         />
-        </Suspense>
-  </main>
+      </Suspense>
+    </main>
   );
 }
