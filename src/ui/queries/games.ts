4913db4e4acb1c834f8a08fe445f892717e1c7f1
YanParoni <paroniyan@gmail.com>
2023-09-11 01:23:25 -0300

    fix: fix build error

--- src/ui/queries/games.ts
+++ src/ui/queries/games.ts
@@ -2,30 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { useDeps } from "../hooks/use-deps";
 import { IGamesGateway } from "../../infra/gateways/contracts/games";
 
-export const useGetGames = () => {
-    const gamesGateway = useDeps<IGamesGateway>('GamesGateway');
-
-    const { data, isLoading, isError, error, refetch, isSuccess } = useQuery({
-        staleTime: 1000 * 60 * 5, 
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-        refetchOnReconnect: false,
-        suspense:true,
-        queryKey: ['getGames'],
-        queryFn: async () => {
-            return await gamesGateway.getGames();
-        },
-    });
-    return {
-        data,
-        isLoading,
-        isError,
-        error,
-        isSuccess,
-        refetch,
-    };
-};
-
 export const useSearchGames = (name: string) => {
     const gamesGateway = useDeps<IGamesGateway>('GamesGateway');
     const { data, isLoading, isError, error, refetch, isSuccess } = useQuery({
