1ba7b85781f1c0519ccccbe90794f7135aca4aa3
YanParoni <paroniyan@gmail.com>
2024-07-17 05:10:55 -0300

    fix: og img

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: 'Playboxd Portfolio by Yan Paroni',
   description: 'A project inspired by Letterboxd and powered by RAWG API.',
   metadataBase: new URL('https://www.linkedin.com/in/yan-paroni/'),
+  twitter:{
+    card: 'summary_large_image'
+  }
 };
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -14,11 +17,15 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
+      <head>
       <meta name="keywords" content="portfolio, frontend, fullstack,nextjs, react,freelance" />
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta property="og:type" content="website"/>
       <meta property="og:title" content="Playboxd" />
       <meta property="og:description" content="Portfolio Project inspired by Letterboxd" />
-      <meta property="og:image" content="/thumbnail.png" />
+      <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="twitter:image" content="/twitter-image.png" />
+      </head>
       <body className="newbg" suppressHydrationWarning={true}>
         {children}
       </body>
